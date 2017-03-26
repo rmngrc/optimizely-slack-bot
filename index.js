45ed0c9b8fe0506dbc54b6f772cce5a9b72a9d45
Ramon Garcia <ramon@dred.com>
2017-03-26 18:08:53 +0200

    :art: Adding support for projects and experiments

---
+++ index.js
@@ -0,0 +1,20 @@
+const Bot = require('slackbots');
+const OptimizelyBot = require('./lib');
+
+
+if (!process.env.UPDATE_CHANNEL) {
+  console.error('Missing `UPDATE_CHANNEL` environment variable');
+  process.exit(-1);
+}
+
+// Interval to listen for changes.
+global.listenInterval = 5000;
+
+// Making the bot global to avoid multiple instanciation.
+global.bot = new Bot(require('./config/bot'));
+
+// Making the function global.
+global.notify = (message) => bot.postMessageToChannel(process.env.UPDATE_CHANNEL, message);
+
+// Running the bot.
+OptimizelyBot.run();
